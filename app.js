eeda4b4febff69dd2a1c66a6e364a1be9ea54175
Kendall Daniels <909283@H2WHK1BEQ6NW.tld>
2022-08-29 08:58:18 -0500

    11 commit

--- app.js
+++ app.js
@@ -5,13 +5,25 @@ function getData() {
     var adj = document.getElementById("adj").value;
     var adverb = document.getElementById("adverb").value;
 
-    console.log ([place, person, verb, adj, adverb])
-    return [place, person, verb, adj, adverb]
+    document.getElementById("sentence").innerHTML = sentences
+    (verb, adverb, person, place, adj);
+
+    //   0  1  2  3  4
 }
 
-function sentences(){
+function sentences(verb, adverb, person, place, adj){
     sentenceArray =[`today I had to ${verb} ${adverb}.
 But right in the middle of it, ${person} came with this ${adj}
-book and wanted to go to ${place} `]
+book and wanted to go to ${place} book and wanted to go ${place}, ${person} was in
+${place} trying to ${verb}. however, ${person} left a
+${adj} shirt behind, while crying ${adverb}. it's been a
+long time since ${person} decided to ${verb} in ${place}.
+what some people said was that ${person} screamed
+this stawberry is ${adj}!! we thought ${person} went crazy.`]
+
+
+ranN = Math.floor(Math.random() * sentenceArray.length) 
+/// 0 to 2
+return sentenceArray[ranN];
 }
 
